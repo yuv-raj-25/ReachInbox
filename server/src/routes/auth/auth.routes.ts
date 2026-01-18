c230c7bfe6412e223a8cbc3d6dc2f85b2fb9255d
yuv-raj-25 <yuvrajchaudhary464@gmail.com>
2026-01-18 21:03:35 +0530

    feat: routes added

--- server/src/routes/auth/auth.routes.ts
+++ server/src/routes/auth/auth.routes.ts
@@ -5,7 +5,7 @@ import { requireAuth } from '../../middleware/auth.middleware.js'
 const router = Router()
 
 router.post('/register', register)
-router.get('/login', login)  
+router.post('/login', login)  
 router.post('/google', googleAuth)
 router.post('/logout', requireAuth,logout)  
 
--- server/src/routes/email/emails.routes.ts
+++ server/src/routes/email/emails.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
-import { scheduleEmail } from '../../controllers/email/emailController.js'
+import { scheduleEmail, getScheduledEmails, getSentEmails } from '../../controllers/email/emailController.js'
 import { requireAuth } from '../../middleware/auth.middleware.js'
 
 const router = Router()
 
 router.post('/bulk-schedule', requireAuth, scheduleEmail)
+router.get('/scheduled', requireAuth, getScheduledEmails)
+router.get('/sent', requireAuth, getSentEmails)
 
 export default router
