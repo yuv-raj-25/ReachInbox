06ba9db5861bb5727b3fd2fe1e9d70a526e13aea
yuv-raj-25 <yuvrajchaudhary464@gmail.com>
2026-01-18 20:57:09 +0530

    all the bullmq redis pg done

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -1,9 +1,14 @@
 import { Redis } from 'ioredis'
-import { env } from './env.js'
 
-
-export const redis = new Redis(env.REDIS_URL)
+export const redis = new Redis(process.env.REDIS_URL!, {
+   maxRetriesPerRequest: null,
+  enableReadyCheck: false
+})
 
 redis.on('connect', () => {
-  console.log('Redis connected')
+  console.log('[REDIS] connected')
+})
+
+redis.on('error', err => {
+  console.error('[REDIS] error', err)
 })
